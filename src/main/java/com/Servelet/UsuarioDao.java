4ec3d103cfdc025e15cdbd4d9e9b25b4261b9753
Nicolas Moisés de Oliveira <nicolas.olimoises79@gmail.com>
2025-08-06 21:39:05 -0300

    criei o usuarioDAO

---
+++ src/main/java/com/Servelet/UsuarioDao.java
@@ -0,0 +1,17 @@
+package com.Servelet;
+
+public interface UsuarioDao {
+	
+	void inserirUsuario(Usuario usuario);
+
+	void deletarUsuario(Usuario usuario);
+
+	void atualizarNomeUsuario(Usuario usuario, String novoNome);
+
+	void atualizarEmailUsuario(Usuario usuario, String novoEmail);
+
+	void atualizarSobrenomeUsuario(Usuario usuario, String novoSobrenome);
+
+	void atualizarSenhaUsuario(Usuario usuario, String novaSenha);
+
+}
